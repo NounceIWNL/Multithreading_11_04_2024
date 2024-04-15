9aac0283ba685feb9e98ef0cb7d2f5b42a00c3d7
gk <officialkepik@gmail.com>
2024-04-15 20:05:23 +0300

    New examples have been added

--- src/basic/ThreadDemo.java
+++ src/basic/ThreadDemo.java
@@ -1,9 +1,24 @@
 package basic;
 
 public class ThreadDemo extends Thread {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Thread : " + Thread.currentThread().getName());
         System.out.println("Creating a child thread");
         Thread thread = new ThreadDemo();
+        System.out.println("Child is starting");
+        thread.start();
+
+        thread.join();
+
+        System.out.println("Finish: " + Thread.currentThread().getName());
+    }
+
+    @Override
+    public void run(){
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Thread: " + i);
+
+        }
+        System.out.println("Finish: " + Thread.currentThread().getName());
     }
 }
