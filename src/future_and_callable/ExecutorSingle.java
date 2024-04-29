7f19340282cbe62b9e3e4da5e9b3b69f20d0e9c5
gk <gk@adamant.local>
2024-04-29 21:24:06 +0300

    New examples have been added

---
+++ src/future_and_callable/ExecutorSingle.java
@@ -0,0 +1,20 @@
+package future_and_callable;
+
+import java.util.concurrent.*;
+
+public class ExecutorSingle {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        //Из предыдущего приложения
+        Callable<String> callable = FutureAndCallable.getCallable();
+
+        System.out.println("Submitting Callable");
+        Future<String> future = executorService.submit(callable);
+        System.out.println("Retrieve the result of the future");
+        System.out.println(future.get());
+        System.out.println(Thread.currentThread().getName() + " is finished");
+
+        executorService.shutdown();
+    }
+} 
